d056fa5cc3631235907b73b5b7b95335935d3ec7
Josué Tille <josue@tille.ch>
2023-02-09 19:49:36 +0100

    Add missing dependancy

--- build_pyenv.sh
+++ build_pyenv.sh
@@ -35,7 +35,7 @@ fi
 # Upgrade system
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libjpeg-dev libpq-dev postgresql libgcrypt20-dev libpq-dev curl
+apt-get install -y build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libjpeg-dev libpq-dev postgresql libgcrypt20-dev libpq-dev curl libkrb5-dev
 
 ## Get last PgAdmin Version
 regex='https://www.postgresql.org/ftp/pgadmin/pgadmin([[:digit:]])/v([[:digit:]]+\.[[:digit:]]+)/pip'
