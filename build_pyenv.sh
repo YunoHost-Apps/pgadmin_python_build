98b1854a64be0ab800e33e38dfac7e781520c1dd
Josué Tille <josue@tille.ch>
2023-06-01 23:57:24 +0200

    Patch wheel to use psycop compilled instead of binary

--- build_pyenv.sh
+++ build_pyenv.sh
@@ -35,7 +35,7 @@ fi
 # Upgrade system
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libjpeg-dev libpq-dev postgresql libgcrypt20-dev libpq-dev curl libkrb5-dev pkg-config
+apt-get install -y build-essential python3-dev libffi-dev python3-pip python3-setuptools sqlite3 libssl-dev python3-venv libjpeg-dev libpq-dev postgresql libgcrypt20-dev libpq-dev curl libkrb5-dev pkg-config zip
 
 ## Get last PgAdmin Version
 regex='https://www.postgresql.org/ftp/pgadmin/pgadmin([[:digit:]])/v([[:digit:]]+\.[[:digit:]]+)/pip'
@@ -66,6 +66,18 @@ source $HOME/.cargo/envs
 mkdir -p $path_to_build
 python3 -m venv --copies $path_to_build
 
+# Patch pip archive
+pip3 download --no-deps pgadmin$app_main_version==$app_sub_version
+rm -rf wheel_archive
+mkdir -p wheel_archive
+pushd wheel_archive
+unzip -q ../pgadmin$app_main_version-$app_sub_version-py3-none-any.whl
+rm -r ../pgadmin$app_main_version-$app_sub_version-py3-none-any.whl
+sed -i  's|psycopg\[binary\]|psycopg[c]|g' pgadmin4-*.dist-info/METADATA
+zip -r -q ../pgadmin$app_main_version-$app_sub_version-py3-none-any.whl *
+popd
+rm -r wheel_archive
+
 # Go in virtualenv
 old_pwd="$PWD"
 pushd $path_to_build
@@ -73,7 +85,9 @@ set +u; source bin/activate; set -u
 
 # Install source and build binary
 pip3 install -I --upgrade pip wheel
-pip3 install -I --upgrade pgadmin$app_main_version==$app_sub_version
+# pip3 install -I --upgrade "psycopg[c]"
+# pip3 install --upgrade pgadmin$app_main_version==$app_sub_version
+pip3 install -I --upgrade $old_pwd/pgadmin$app_main_version-$app_sub_version-py3-none-any.whl
 pip3 freeze > $old_pwd/pgadmin_${APP_VERSION}-$(lsb_release --codename --short)-build${release_number}_requirement.txt
 
 # Quit virtualenv
