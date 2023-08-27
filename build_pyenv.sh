39d890e7e3c47e0151d0d5d98a3adda7b6bb7a00
Josué Tille <josue@tille.ch>
2023-08-27 23:16:07 +0200

    Fix requirement.txt generation

--- build_pyenv.sh
+++ build_pyenv.sh
@@ -88,7 +88,7 @@ pip3 install -I --upgrade pip wheel
 # pip3 install -I --upgrade "psycopg[c]"
 # pip3 install --upgrade pgadmin$app_main_version==$app_sub_version
 pip3 install -I --upgrade $old_pwd/pgadmin$app_main_version-$app_sub_version-py3-none-any.whl
-pip3 freeze > $old_pwd/pgadmin_${APP_VERSION}-$(lsb_release --codename --short)-build${release_number}_requirement.txt
+pip3 freeze | sed "s|pgadmin4\s*@\s*file:.*|pgadmin4==$app_sub_version|g" > $old_pwd/pgadmin_${APP_VERSION}-$(lsb_release --codename --short)-build${release_number}_requirement.txt
 # Fix requirement for pgadmin binary
 sed -i "s|pgadmin4==.*|pgadmin4==$app_sub_version|g" $old_pwd/pgadmin_${APP_VERSION}-$(lsb_release --codename --short)-build${release_number}_requirement.txt
 
